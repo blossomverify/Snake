d290b1154c3d380ed2d68ab417bf335f6cb30816
Murilo <15041329630@ulife.com.br>
2026-07-23 19:07:18 -0300

    bibliotecas, largura e altura e cores adicionadas

---
+++ main.py
@@ -0,0 +1,42 @@
+# Configurações iniciais do projeto
+
+import pygame
+import random
+
+pygamen.init()
+pygame.display.set_caption("Snake Game")
+
+largura, altura = 600, 400
+pygame.display.set_mode((largura, altura))
+
+# cores
+preta = (0, 0, 0)
+branco = (255, 255, 255)
+vermelho = (255, 0, 0)
+verde = (0, 255, 0)
+
+# parametros do jogo
+
+
+
+
+
+
+
+# criar um loop infinito para manter o programa em execução
+
+# Desenhar os objetos na tela
+# cobrinha
+# pontos 
+# comida
+
+
+# criar a logica de terminar o jogo
+# como o jogador temina o jogo:
+# cobra bateu na parede
+# cobra bateu na propria cauda
+
+# pegar interações do usuário
+# fechou a tela
+# apertou alguma tecla
+# 
